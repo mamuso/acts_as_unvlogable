f47340012dfe29b49c2d88c9886f34da6bfd7359
manuel muñoz solera <mamusino@gmail.com>
2009-01-04 23:22:39 +0100

    adding :nosearchbox option to embed_html on the youtube library

--- lib/vg_youtube.rb
+++ lib/vg_youtube.rb
@@ -26,8 +26,11 @@ class VgYoutube
     @details.media_content.first.url if @details.noembed == false
   end
   
+  # options 
+  #   :nosearchbox => true | removes the searchbox on the player
+  # 
   def embed_html(width=425, height=344, options={})
-    "<object width='#{width}' height='#{height}'><param name='movie' value='#{embed_url}&fs=1'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param><embed src='#{embed_url}&fs=1' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='#{width}' height='#{height}'></embed></object>" if @details.noembed == false
+    "<object width='#{width}' height='#{height}'><param name='movie' value='#{embed_url}&fs=1#{'&searchbox=0' if options[:nosearchbox]}'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param><embed src='#{embed_url}&fs=1#{'&searchbox=0' if options[:nosearchbox] == true}' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='#{width}' height='#{height}'></embed></object>" if @details.noembed == false
   end
   
   
