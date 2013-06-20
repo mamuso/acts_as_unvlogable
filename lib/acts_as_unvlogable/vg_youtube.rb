17b46c71b3dff143f651db644f304428b1793f67
David J. Brenes <david.brenes@simplelogica.net>
2013-06-20 11:33:21 +0200

    Adding params to youtube (so we can pass wmode=opaque)

--- lib/acts_as_unvlogable/vg_youtube.rb
+++ lib/acts_as_unvlogable/vg_youtube.rb
@@ -35,9 +35,9 @@ class VgYoutube
   #   You can read more about the youtube player options in 
   #   http://code.google.com/intl/en/apis/youtube/player_parameters.html
   #   Use them in options (ex {:rel => 0, :color1 => '0x333333'})
-  # 
-  def embed_html(width=425, height=344, options={})
-    "<object width='#{width}' height='#{height}'><param name='movie' value='#{embed_url}#{options.map {|k,v| "&#{k}=#{v}"}}'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param><embed src='#{embed_url}#{options.map {|k,v| "&#{k}=#{v}"}}' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='#{width}' height='#{height}'></embed></object>" if @details.noembed == false
+  #
+  def embed_html(width=425, height=344, options={}, params={})
+    "<object width='#{width}' height='#{height}'><param name='movie' value='#{embed_url}#{options.map {|k,v| "&#{k}=#{v}"}}'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>#{params.map{|k,v|"<param name='#{k}' value='#{v}'></param>"}.join}<embed src='#{embed_url}#{options.map {|k,v| "&#{k}=#{v}"}}' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='#{width}' height='#{height}' #{params.map {|k,v| "&#{k}=#{v}"}.join(" ")}></embed></object>" if @details.noembed == false
   end
   
   
